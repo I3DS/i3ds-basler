72188b46022895c6f1a09a5eb66e31d2c0d87179
Sigurd M. Albrektsen <sigurd.albrektsen@sintef.no>
2018-07-13 14:45:37 +0200

    Changed default bsp and removed unused param

--- petamake.sh
+++ petamake.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BSP_NAME=i3ds_bsp
+BSP_NAME=i3ds-bsp
 BSP_ROOT=/opt/bsp/$BSP_NAME/build/tmp/sysroots/plnx_aarch64
 
 mkdir -p build_petalinux
@@ -8,6 +8,5 @@ cd build_petalinux
 cmake .. -DCMAKE_C_COMPILER="aarch64-linux-gnu-gcc" \
       -DCMAKE_CXX_COMPILER="aarch64-linux-gnu-c++" \
       -DCMAKE_FIND_ROOT_PATH="$BSP_ROOT" \
-      -DBUILD_EMBEDDED=ON \
-      -DEBUS_INCLUDE_DIR="/usr/include/ebus"
+      -DBUILD_EMBEDDED=ON
 make -j4
