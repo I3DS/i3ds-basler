7116e46fc6bf6c97f6d632082b537214dbb9eaac
Espen Helle <espen.helle@sintef>
2018-10-18 15:16:29 +0200

    Adds C++11 override

--- include/basler_camera.hpp
+++ include/basler_camera.hpp
@@ -34,62 +34,62 @@ public:
 protected:
 
   // Camera control
-  virtual void Open();
-  virtual void Close();
-  virtual void Start();
-  virtual void Stop();
+  virtual void Open() override;
+  virtual void Close() override;
+  virtual void Start() override;
+  virtual void Stop() override;
 
   // Set internal trigger to the given period.
-  virtual bool setInternalTrigger(int64_t period_us);
+  virtual bool setInternalTrigger(int64_t period_us) override;
 
   // Sensor width and height
-  virtual int64_t getSensorWidth() const;
-  virtual int64_t getSensorHeight() const;
+  virtual int64_t getSensorWidth() const override;
+  virtual int64_t getSensorHeight() const override;
 
   // Region of interest
-  virtual bool isRegionSupported() const;
+  virtual bool isRegionSupported() const override;
 
-  virtual int64_t getRegionWidth() const;
-  virtual int64_t getRegionHeight() const;
-  virtual int64_t getRegionOffsetX() const;
-  virtual int64_t getRegionOffsetY() const;
+  virtual int64_t getRegionWidth() const override;
+  virtual int64_t getRegionHeight() const override;
+  virtual int64_t getRegionOffsetX() const override;
+  virtual int64_t getRegionOffsetY() const override;
 
-  virtual void setRegionWidth(int64_t width);
-  virtual void setRegionHeight(int64_t height);
-  virtual void setRegionOffsetX(int64_t offset_x);
-  virtual void setRegionOffsetY(int64_t offset_y);
+  virtual void setRegionWidth(int64_t width) override;
+  virtual void setRegionHeight(int64_t height) override;
+  virtual void setRegionOffsetX(int64_t offset_x) override;
+  virtual void setRegionOffsetY(int64_t offset_y) override;
 
   // Shutter time in microseconds
-  virtual int64_t getShutter() const;
-  virtual int64_t getMaxShutter() const;
-  virtual int64_t getMinShutter() const;
-  virtual void setShutter(int64_t shutter_us);
+  virtual int64_t getShutter() const override;
+  virtual int64_t getMaxShutter() const override;
+  virtual int64_t getMinShutter() const override;
+  virtual void setShutter(int64_t shutter_us) override;
 
-  virtual bool isAutoShutterSupported() const;
+  virtual bool isAutoShutterSupported() const override;
 
-  virtual bool getAutoShutterEnabled() const;
-  virtual void setAutoShutterEnabled(bool enable);
+  virtual bool getAutoShutterEnabled() const override;
+  virtual void setAutoShutterEnabled(bool enable) override;
 
-  virtual int64_t getAutoShutterLimit() const;
-  virtual int64_t getMaxAutoShutterLimit() const;
-  virtual int64_t getMinAutoShutterLimit() const;
-  virtual void setAutoShutterLimit(int64_t shutter_limit);
+  virtual int64_t getAutoShutterLimit() const override;
+  virtual int64_t getMaxAutoShutterLimit() const override;
+  virtual int64_t getMinAutoShutterLimit() const override;
+  virtual void setAutoShutterLimit(int64_t shutter_limit) override;
 
   // Gain in decibel
-  virtual double getGain() const;
-  virtual double getMaxGain() const;
-  virtual double getMinGain() const;
-  virtual void setGain(double gain);
+  virtual double getGain() const override;
+  virtual double getMaxGain() const override;
+  virtual double getMinGain() const override;
+  virtual void setGain(double gain) override;
 
-  virtual bool isAutoGainSupported() const;
+  virtual bool isAutoGainSupported() const override;
 
-  virtual bool getAutoGainEnabled() const;
-  virtual void setAutoGainEnabled(bool enable);
+  virtual bool getAutoGainEnabled() const override;
+  virtual void setAutoGainEnabled(bool enable) override;
 
-  virtual double getAutoGainLimit() const;
-  virtual double getMaxAutoGainLimit() const;
-  virtual double getMinAutoGainLimit() const;
-  virtual void setAutoGainLimit(double gain_limit);
+  virtual double getAutoGainLimit() const override;
+  virtual double getMaxAutoGainLimit() const override;
+  virtual double getMinAutoGainLimit() const override;
+  virtual void setAutoGainLimit(double gain_limit) override;
 
 private:
 
